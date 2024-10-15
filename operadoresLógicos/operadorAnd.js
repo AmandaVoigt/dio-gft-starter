136e88723c7b823fd34d0776db4f71e1a68612f8
Amanda Voigt <amanda02_vg@hotmail.com>
2024-10-15 14:14:20 -0300

    Finalizando o módulo de Operadores Lógicos

---
+++ operadoresLógicos/operadorAnd.js
@@ -0,0 +1,11 @@
+// AND &&
+let idade = 18
+let vistoVerificado = true
+let resultado = ((idade >= 18) && (vistoVerificado === true))
+console.log(resultado)
+
+// 100 moedas coletadas e 1 item estrela
+let moedasColetadas = 100
+let item = "estrela"
+resultado = (moedasColetadas === 100) && (item === "estrela")
+console.log(resultado)
